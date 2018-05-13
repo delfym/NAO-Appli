e4847c2482a240ea10d2b573d7d2c2986000c118
delphine.millot <delfymillot@gmail.com>
2018-05-13 23:15:38 +0200

    Mise en place de lapage d'accueil

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: delphinemillotpedrero
+ * Date: 11/05/2018
+ * Time: 23:21
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PagesController
+{
+    /**
+     * @Route("/")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function index(Environment $twig){
+        return new Response($twig->render('pages/index.html.twig'));
+    }
+
+    /**
+     * @Route("/home", name="home")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function home(Environment $twig){
+        return new Response($twig->render('pages/index.html.twig'));
+    }
+
+    /**
+     * @Route("/test")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function test(Environment $twig){
+       // return new Response('test');
+        return new Response($twig->render('pages/index.html.twig'));
+    }
+
+    /**
+     * @Route("/sidebarLeft")
+     * @param Environment $twig
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function sidebarLeft(Environment $twig){
+        return new Response($twig->render('pages/sidebar-left.html.twig'));
+    }
+}
